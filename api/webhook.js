f8273facd2bb859995ad6e48c47480d83aa45b54
IvoPacca <145924127+IvoPacca@users.noreply.github.com>
2026-06-15 23:36:25 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -13,8 +13,6 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
 
-  return res.status(200).json({
-    success: true,
-    method: req.method
-  });
+  return res.redirect(302, '/sucesso.html');
+
 }
